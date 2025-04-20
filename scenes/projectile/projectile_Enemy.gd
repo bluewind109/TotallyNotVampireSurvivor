8ac4d4bf2773004347e435d85e8dd717a0606285
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2025-04-21 02:05:44 +0700

    Set enemy projectile to vanish on hitting player

--- scenes/projectile/projectile_Enemy.gd
+++ scenes/projectile/projectile_Enemy.gd
@@ -17,10 +17,20 @@ func _physics_process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.is_in_group("Player")):
-		print("[ProjectileEnemy] _on_body_entered: ", damage)
+		# print("[ProjectileEnemy] _on_body_entered: ", damage)
 		# body = body as Enemy
 		# body.take_damage(damage)
 		# body.add_knockback(direction * 75) # stronger knockback effect
+		pass
+	pass
+
+func _on_area_entered(area: Area2D) -> void:
+	if (area.is_in_group("Player")):
+		# print("[ProjectileEnemy] func _on_area_entered(area: Area2D) -> void: ", damage)
+		# body = body as Enemy
+		# body.take_damage(damage)
+		# body.add_knockback(direction * 75) # stronger knockback effect
+		queue_free()
 		pass
 	pass
 
