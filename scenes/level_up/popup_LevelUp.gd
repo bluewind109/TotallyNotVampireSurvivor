9e16d8686b4e65cb84ce9ad8e16fb1eed93b1c8b
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2025-06-16 07:48:47 +0700

    Working on popup level up animation

--- scenes/level_up/popup_LevelUp.gd
+++ scenes/level_up/popup_LevelUp.gd
@@ -4,19 +4,26 @@ class_name PopupLevelUp
 @export var particle_level_up: GPUParticles2D
 @export var upgrade_container: HBoxContainer
 @export var upgrade_slot_prefab: PackedScene
+@export var anim_player: AnimationPlayer
 
 @export var upgrade_type: Array[BaseStrategy]
 
 var upgrade_number: int = 3
+var is_popup_show_anim_finished: bool = false
+var is_init: bool = false
 
 func _ready() -> void:
 	SignalManager.on_level_up.connect(show_panel)
 	SignalManager.on_stat_upgrade_selected.connect(on_upgrade_selected)
 
 func show_panel():
+	is_popup_show_anim_finished = false
+	is_init = false
 	if not is_node_ready():
 		await ready
 
+	print("show_panel 1")
+	anim_player.play.call_deferred("popup_show")
 	for child in upgrade_container.get_children():
 		child.free()
 	show()
@@ -31,12 +38,19 @@ func show_panel():
 	# 	UpgradeConfig.get_random_upgrades_with_rarity(
 	# 	upgrade_number, rarities)
 
+	await anim_player.animation_finished
+	print("show_panel 2")
+
 	var upgrades: Array[int] = UpgradeConfig.get_random_upgrades_with_rarity(
 		upgrade_number, rarities)
 	# print(upgrades)
 	for upgrade in upgrades:
 		show_upgrade(UpgradeConfig.UPGRADE_TYPE.values()[upgrade])
+		await get_tree().create_timer(0.15).timeout
 		# print(upgrade)
+	
+	is_init = true
+	print("show_panel 3")
 
 	# var upgrades = UpgradeConfig.get_random_upgrades(upgrade_number)
 	# for upgrade in upgrades.size():
@@ -57,3 +71,12 @@ func hide_panel():
 	hide()
 	particle_level_up.emitting = false
 	get_tree().paused = false
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if (anim_name == "popup_show"):
+		is_popup_show_anim_finished = true
+		anim_player.animation_finished.emit()
+
+
+	pass # Replace with function body.
