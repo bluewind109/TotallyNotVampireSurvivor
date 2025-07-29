b0fe243ba126b5dceb454cce3f2557e97f0fcb1a
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2025-07-29 18:52:59 +0700

    Update

--- scenes/character/enemy/enemy.gd
+++ scenes/character/enemy/enemy.gd
@@ -118,7 +118,7 @@ func init_spawn(
 	position = pos
 	set_rank(_rank)
 	sprite.texture = texture
-	print("init_spawn")
+	# print("init_spawn")
 	component_hitbox.take_damage.connect(take_damage)
 	component_health.init(health)
 	component_barrier.init(health) # test
